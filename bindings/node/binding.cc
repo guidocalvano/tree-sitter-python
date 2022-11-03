f63985b28185949ecc25008dd0fce074e88d0c41
Guido Sales Calvano <guidocalvano@gmail.com>
2022-11-03 14:01:40 +0100

    context_aware

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -10,19 +10,37 @@ namespace {
 
 NAN_METHOD(New) {}
 
-void Init(Local<Object> exports, Local<Object> module) {
-  Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
-  tpl->SetClassName(Nan::New("Language").ToLocalChecked());
-  tpl->InstanceTemplate()->SetInternalFieldCount(1);
+// void InitAll(Local<Object> exports) { //, Local<Object> module) {
+//   Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
+//   tpl->SetClassName(Nan::New("Language").ToLocalChecked());
+//   tpl->InstanceTemplate()->SetInternalFieldCount(1);
+//
+//   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
+//   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
+//   Nan::SetInternalFieldPointer(instance, 0, tree_sitter_python());
+//
+//   Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("python").ToLocalChecked());
+//   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
+// }
+// NAN_MODULE_WORKER_ENABLED(tree_sitter_python_binding, InitAll)
+//
 
-  Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
-  Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_python());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("python").ToLocalChecked());
-  Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
+extern "C" NODE_MODULE_EXPORT void
+NODE_MODULE_INITIALIZER(Local<Object> exports,
+                        Local<Object> module,
+                        Local<Context> context) {
+  /* Perform addon initialization steps here. */
+    Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
+    tpl->SetClassName(Nan::New("Language").ToLocalChecked());
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
+
+    Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
+    Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
+    Nan::SetInternalFieldPointer(instance, 0, tree_sitter_python());
+
+    Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("python").ToLocalChecked());
+    Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
-
-NODE_MODULE(tree_sitter_python_binding, Init)
 
 }  // namespace
